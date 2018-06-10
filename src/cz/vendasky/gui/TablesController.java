4dfb9ea9bb5ac98ab9bd94dd75df841531c131ce
Václav Pavlíček <vaclav@pavlicek.cz>
2018-06-10 23:28:17 +0200

    Add button for table describe

--- src/cz/vendasky/gui/TablesController.java
+++ src/cz/vendasky/gui/TablesController.java
@@ -2,16 +2,26 @@ package cz.vendasky.gui;
 
 import cz.vendasky.Model;
 import cz.vendasky.MySQLConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.net.URL;
@@ -27,6 +37,58 @@ public class TablesController implements Initializable {
 
     @FXML
     private ListView tablesListView;
+
+    static class TableCell extends ListCell<String> {
+        HBox hbox = new HBox();
+        Label label = new Label("");
+        Pane pane = new Pane();
+        Button button = new Button("*");
+        String lastItem;
+        Model model;
+
+        public TableCell(Model model) {
+            super();
+            hbox.getChildren().addAll(label, pane, button);
+            HBox.setHgrow(pane, Priority.ALWAYS);
+            this.model = model;
+            button.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    try {
+                        model.setSelectedTable(lastItem);
+                        MySQLConnection instance = MySQLConnection.getInstance();
+                        Connection connection = instance.getConnection();
+                        Statement statement = connection.createStatement();
+                        System.out.println("DESCRIBE " + model.getSelectedDatabase() + "." + model.getSelectedTable());
+                        ResultSet rs = statement.executeQuery("DESCRIBE " + model.getSelectedDatabase() + "." + model.getSelectedTable());
+                        while (rs.next()) {
+                            System.out.println(rs.getString("field") + " " + rs.getString("type") + " " + rs.getString("null") + " " + rs.getString("key") + " " + rs.getString("default") + " " + rs.getString("extra"));
+                        }
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(lastItem + " : " + event);
+                }
+            });
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+            setText(null);  // No text in label of super class
+            if (empty) {
+                lastItem = null;
+                setGraphic(null);
+            } else {
+                lastItem = item;
+                label.setText(item!=null ? item : "<null>");
+                setGraphic(hbox);
+            }
+        }
+    }
 
     @FXML
     public void goBack(ActionEvent actionEvent) throws IOException {
@@ -79,11 +141,15 @@ public class TablesController implements Initializable {
             while (rs.next()) {
                 tables.add(rs.getString("table_name"));
             }
-            System.out.println(tables);
-            this.tablesListView.getItems().addAll(tables);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            ObservableList<String> list = FXCollections.observableArrayList(tables);
+            tablesListView.getItems().addAll(list);
+            tablesListView.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+                @Override
+                public ListCell<String> call(ListView<String> param) {
+                    return new TableCell(model);
+                }
+            });
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
